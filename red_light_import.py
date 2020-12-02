b5e9469a551dc70e78b70f4120e748d838de3c5b
jsmarier <marierjs@gmail.com>
2020-12-02 14:54:21 -0500

    Update and rename red_light_import to red_light_import.py

--- red_light_import.py
+++ red_light_import.py
@@ -1,6 +1,8 @@
 import pandas as pd
+
 test = pd.read_csv("https://opendata.arcgis.com/datasets/b053664e31a846078bea540c5224e34c_0.csv")
 average = (test.mean()
-           .loc["TOTAL_VIOLATIONS"]
+           .reset_index()
+           .iloc[5:16]
           )
 average
